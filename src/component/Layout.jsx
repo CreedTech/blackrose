ce8c39532ffd2e3a3fb09a64d7d9f1903d693731
creedtech <ayodeleayoolaoke4@gmail.com>
2025-06-09 18:56:40 +0100

    refactor: Update loading condition in Layout component to display spinner when loading

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -155,7 +155,7 @@ const Layout = () => {
   // </div>
   return (
     <div>
-      {!isLoading && (
+      {isLoading && (
         <div className="flex items-center justify-center h-screen bg-black">
           <div className="animate-spin rounded-full h-16 w-16 md:h-32 md:w-32 border-t-2 border-b-2 border-primary"></div>
         </div>
